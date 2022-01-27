646849048abc644589854128bac5212a5da6feb4
Michael Lisboa <michael.lisboa@gmail.com>
2022-01-27 17:54:35 +0700

    updating the story

--- src/components/HyperSwiper/Card.js
+++ src/components/HyperSwiper/Card.js
@@ -66,7 +66,7 @@ const Card = ({bind, current, active, card, gone, rot, scale, height: viewportHe
                             }
                             </p>
                         <p
-                            style={{lineHeight: "1.25rem", overflowY: "scroll"}}
+                            style={{lineHeight: "1.35rem"}}
                             className="uk-padding-remove-top uk-margin-remove-top uk-flex uk-flex-top">{card.language.jive.story}</p>
                     </a.div>
                 </div>
